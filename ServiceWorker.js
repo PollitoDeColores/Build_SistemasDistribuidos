17bc738710f085a6cf843fdb30de8e3d5cb08bc0
iSekkachi <contact.coloredchick@gmail.com>
2024-09-10 22:00:15 -0500

    Initial Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Sistemas_Distribuidos-1.0";
+const contentToCache = [
+    "Build/Builds_SistemasDistribuidos.loader.js",
+    "Build/Builds_SistemasDistribuidos.framework.js",
+    "Build/Builds_SistemasDistribuidos.data",
+    "Build/Builds_SistemasDistribuidos.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
